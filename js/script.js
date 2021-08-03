45d2a3f93b622e2e6335bc5c1cb71a081fa0b37a
Pramodhgg <48713723+Pramodhgg@users.noreply.github.com>
2021-08-03 15:31:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ tl.to(".animated_slider1", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".animated_intro_page", { y: "-100%", duration: 0.2 }, "-=1");
 tl.fromTo(".nav_bar", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".animae", { opacity: 0 }, { opacity: 1, duration: 1, stagger: 0.5 });
+tl.fromTo(
+  ".box-area",
+  { display: "none", opacity: "0" },
+  { display: "block", opacity: "1", duration: 0.4 },
+  "-=4"
+);
+tl.fromTo(
+  ".header_section",
+  { display: "none" },
+  { display: "block", duration: 1 },
+  "-=6.5"
+);
+
