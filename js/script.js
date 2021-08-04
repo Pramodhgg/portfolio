527131bd5b302b913677ff00540048b785b1b6e0
Pramodhgg <48713723+Pramodhgg@users.noreply.github.com>
2021-08-04 15:52:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
+let windows = window.pageYOffset;
 let about = document.querySelector("#about");
 let project = document.querySelector("#project");
 let contact = document.querySelector("#contact");
+about.offsetTop = 0;
+project.offsetTop = 0;
+contact.offsetTop = 0;
+console.log(windows);
+if (windows) {
+  window.pageYOffset = 0;
+}
+document.querySelector(".about").removeAttribute("id", "active");
+document.querySelector(".project").removeAttribute("id", "active");
+document.querySelector(".contact").removeAttribute("id", "active");
+
+function refreshPage() {
+  window.scrollTo(0, 0);
+}
+
+window.onload = (event) => {
+  setTimeout(refreshPage, 3000);
+};
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { x: "0%", opacity: "1", duration: 1, stagger: 0.25 });
 tl.to(".animated_slider1", { y: "-100%", duration: 1.5, delay: 0.5 });
+
 tl.to(".animated_intro_page", { y: "-100%", duration: 0.2 }, "-=1");
 tl.fromTo(".nav_bar", { opacity: 0 }, { opacity: 1, duration: 1 });
+
 tl.fromTo(".animae", { opacity: 0 }, { opacity: 1, duration: 1, stagger: 0.5 });
 tl.fromTo(
   ".box-area",
@@ -16,36 +37,37 @@ tl.fromTo(
   "-=4"
 );
 tl.fromTo(
+  ".left_sticky_list",
+  { opacity: 0 },
+  { opacity: 1, duration: 1, stagger: 0.5 }
+);
+
+tl.fromTo(
   ".header_section",
   { display: "none" },
   { display: "block", duration: 1 },
   "-=6.5"
 );
 
-document.querySelector(".about").removeAttribute("id", "active");
-document.querySelector(".project").removeAttribute("id", "active");
-document.querySelector(".contact").removeAttribute("id", "active");
-window.onload = (event) => {
-  console.log("reloaded");
-  window.scrollTo(0, 0);
-  document.querySelector(".about").removeAttribute("id", "active");
-  document.querySelector(".project").removeAttribute("id", "active");
-  document.querySelector(".contact").removeAttribute("id", "active");
-};
 window.addEventListener("scroll", () => {
-  let windows = window.pageYOffset;
-
-  if (about.offsetTop <= windows && project.offsetTop > windows) {
+  windows = window.pageYOffset;
+  if (about.offsetTop > windows) {
+    document.querySelector(".project").removeAttribute("id", "active");
+    document.querySelector(".about").removeAttribute("id", "active");
+    document.querySelector(".contact").removeAttribute("id", "active");
+  } else if (about.offsetTop - 100 <= windows && project.offsetTop > windows) {
     document.querySelector(".about").setAttribute("id", "active");
 
     document.querySelector(".project").removeAttribute("id", "active");
     document.querySelector(".contact").removeAttribute("id", "active");
-  } else if (project.offsetTop <= windows && contact.offsetTop > windows) {
+  } else if (
+    project.offsetTop - 300 <= windows &&
+    contact.offsetTop > windows
+  ) {
     document.querySelector(".project").setAttribute("id", "active");
     document.querySelector(".about").removeAttribute("id", "active");
     document.querySelector(".contact").removeAttribute("id", "active");
-  } else if (contact.offsetTop <= windows) {
-    console.log("iam in contact fcckkk !");
+  } else if (contact.offsetTop - 100 <= windows) {
     document.querySelector(".contact").setAttribute("id", "active");
     document.querySelector(".about").removeAttribute("id", "active");
     document.querySelector(".project").removeAttribute("id", "active");
